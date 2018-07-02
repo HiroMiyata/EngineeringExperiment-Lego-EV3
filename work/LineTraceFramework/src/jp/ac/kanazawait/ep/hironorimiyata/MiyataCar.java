c27ae9bc6f6d087131b6973ca70e34a5127314f2
HiroMiyata <b1633102@planet.kanazawa-it.ac.jp>
2018-07-02 15:47:51 +0900

    17.5秒をマークしたプログラム

--- work/LineTraceFramework/src/jp/ac/kanazawait/ep/hironorimiyata/MiyataCar.java
+++ work/LineTraceFramework/src/jp/ac/kanazawait/ep/hironorimiyata/MiyataCar.java
@@ -1,7 +1,6 @@
 package jp.ac.kanazawait.ep.hironorimiyata;
 
 import jp.ac.kanazawait.ep.mmotoki.abst.AbstCar;
-import jp.ac.kanazawait.ep.mmotoki.driver.SimpleDriver;
 import jp.ac.kanazawait.ep.mmotoki.navigator.SimpleLeftEdgeTracer;
 import jp.ac.kanazawait.ep.mmotoki.util.CheckerColorSensor;
 import jp.ac.kanazawait.ep.mmotoki.util.EV3Logger;
@@ -11,7 +10,7 @@ import lejos.robotics.Color;
 public class MiyataCar extends AbstCar {
 	public MiyataCar() {
 		this.checkerColorSensor = CheckerColorSensor.getInstance();
-		this.driver = new SimpleDriver("B", "C");
+		this.driver = new MiyataDriver("B", "C");
 		this.navigator = new SimpleLeftEdgeTracer();
 		this.logger = EV3Logger.getInstance();
 		this.logger.setChecker(checkerColorSensor);
@@ -22,19 +21,22 @@ public class MiyataCar extends AbstCar {
 	@Override
 	public void run() {
 		this.start();
-		int sen = 0;
+		boolean sel = true;
+		boolean ch =true;
 		long time = System.currentTimeMillis();
 		long count = System.currentTimeMillis();
 		while(!Button.ESCAPE.isDown() && this.checkerColorSensor.getColorId() != Color.RED) {
 			
-			if(sen % 2 == 0 ) {
+			if(sel) {
 				this.navigator = new RightEdgeTracer();
+				ch = false;
 			} else {
 				this.navigator = new SimpleLeftEdgeTracer();
+				ch = true;
 			}
 			time = System.currentTimeMillis();
 			if(this.checkerColorSensor.getColorId() == Color.GREEN && count+1000 < time ) {
-				sen++;
+				sel = ch;
 				count = System.currentTimeMillis();
 			}
 			
